fd93902ecc79b5df0b734a11a6fb9a587adf7e46
pooler22 <rudypawel12@gmail.com>
2016-06-12 10:31:36 +0200

    prepare class knn and knm

---
+++ src/KNMClassifier.java
@@ -0,0 +1,13 @@
+/**
+ * Created by pooler.
+ */
+public class KNMClassifier extends Classifier{
+    public KNMClassifier(double[][] dataSet, int[] ClassLabels, int[] SampleCount) {
+        super(dataSet, ClassLabels, SampleCount);
+    }
+
+    @Override
+    double execute() {
+        return 0;
+    }
+}
---
+++ src/KNNClassifier.java
@@ -0,0 +1,13 @@
+/**
+ * Created by pooler.
+ */
+public class KNNClassifier extends Classifier{
+    public KNNClassifier(double[][] dataSet, int[] ClassLabels, int[] SampleCount) {
+        super(dataSet, ClassLabels, SampleCount);
+    }
+
+    @Override
+    double execute() {
+        return 0;
+    }
+}
