ed42042b2f5607254ddeeaab7bcfab564f91e0e9
pooler22 <rudypawel12@gmail.com>
2016-06-14 00:07:50 +0200

    hard refactoring

--- src/Data.java
+++ src/Data.java
@@ -1,14 +1,131 @@
 import Jama.Matrix;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by pooler on 04.05.2016.
  */
-public class Data {
-    public static double[][] projectSamples(Matrix FOld, Matrix TransformMat) {
+class Data {
+
+    String InData; // dataset from a text file will be placed here
+    int FeatureCount = 0;
+    int[] ClassLabels, SampleCount;
+    double[] vec;
+    double[][] F, FNew; // original feature matrix and transformed feature matrix
+
+    void getDatasetParameters() throws Exception {
+        // based on data stored in InData determine: class count and names, number of samples
+        // and number of features; set the corresponding variables
+        String stmp = InData, saux;
+        // analyze the first line and get feature count: assume that number of features
+        // equals number of commas
+        saux = InData.substring(InData.indexOf(',') + 1, InData.indexOf('$'));
+        if (saux.length() == 0) {
+            throw new Exception("The first line is empty");
+        }
+        // saux stores the first line beginning from the first comma
+        int count = 0;
+        while (saux.indexOf(',') > 0) {
+            saux = saux.substring(saux.indexOf(',') + 1);
+            count++;
+        }
+        FeatureCount = count + 1; // the first parameter
+        // Determine number of classes, class names and number of samples per class
+        boolean New;
+        int index = -1;
+        List<String> NameList = new ArrayList<>();
+        List<Integer> CountList = new ArrayList<>();
+        List<Integer> LabelList = new ArrayList<>();
+        while (stmp.length() > 1) {
+            saux = stmp.substring(0, stmp.indexOf(' '));
+            New = true;
+            index++; // new class index
+            for (int i = 0; i < NameList.size(); i++) {
+                if (saux.equals(NameList.get(i))) {
+                    New = false;
+                    index = i; // class index
+                }
+            }
+            if (New) {
+                NameList.add(saux);
+                CountList.add(0);
+            } else {
+                CountList.set(index, CountList.get(index) + 1);
+            }
+            LabelList.add(index); // class index for current row
+            stmp = stmp.substring(stmp.indexOf('$') + 1);
+        }
+        // based on results of the above analysis, create variables
+        String[] classNames = new String[NameList.size()];
+        for (int i = 0; i < classNames.length; i++) {
+            classNames[i] = NameList.get(i);
+        }
+        SampleCount = new int[CountList.size()];
+        for (int i = 0; i < SampleCount.length; i++) {
+            SampleCount[i] = CountList.get(i) + 1;
+        }
+        ClassLabels = new int[LabelList.size()];
+        for (int i = 0; i < ClassLabels.length; i++) {
+            ClassLabels[i] = LabelList.get(i);
+        }
+    }
+
+    void fillFeatureMatrix() throws Exception {
+        // having determined array size and class labels, fills in the feature matrix
+        int n = 0;
+        String saux, stmp = InData;
+        for (int aSampleCount : SampleCount) {
+            n += aSampleCount;
+        }
+        if (n <= 0) {
+            throw new Exception("no samples found");
+        }
+        F = new double[FeatureCount][n]; // samples are placed column-wise
+        for (int j = 0; j < n; j++) {
+            saux = stmp.substring(0, stmp.indexOf('$'));
+            saux = saux.substring(stmp.indexOf(',') + 1);
+            for (int i = 0; i < FeatureCount - 1; i++) {
+                F[i][j] = Double.parseDouble(saux.substring(0, saux.indexOf(',')));
+                saux = saux.substring(saux.indexOf(',') + 1);
+            }
+            F[FeatureCount - 1][j] = Double.parseDouble(saux);
+            stmp = stmp.substring(stmp.indexOf('$') + 1);
+        }
+    }
+
+    String readDataSet(PR_GUI pr_gui) {
+        String name = "";
+        String s_tmp, s_out = "";
+        JFileChooser jfc = new JFileChooser();
+        jfc.setCurrentDirectory(new File(".."));
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Datasets - plain text files", "txt");
+        jfc.setFileFilter(filter);
+        if (jfc.showOpenDialog(pr_gui) == JFileChooser.APPROVE_OPTION) {
+            try {
+                try (BufferedReader br = new BufferedReader(new FileReader(jfc.getSelectedFile()))) {
+                    while ((s_tmp = br.readLine()) != null) {
+                        s_out += s_tmp + '$';
+                    }
+                }
+                name = jfc.getSelectedFile().getName();
+            } catch (Exception ignored) {
+            }
+        }
+        InData = s_out;
+        return name;
+    }
+
+    static double[][] projectSamples(Matrix FOld, Matrix TransformMat) {
         return (FOld.transpose().times(TransformMat)).transpose().getArrayCopy();
     }
 
-    public static double[][] centerAroundMean(double[][] M) {
+    static double[][] centerAroundMean(double[][] M) {
         double[] mean = new double[M.length];
         for (int i = 0; i < M.length; i++) {
             for (int j = 0; j < M[0].length; j++) {
@@ -26,10 +143,10 @@ public class Data {
         return M;
     }
 
-    public static Matrix computeCovarianceMatrix(double[][] m) {
+    static Matrix computeCovarianceMatrix(double[][] m) {
         Matrix M = new Matrix(m);
         Matrix MT = M.transpose();
-        Matrix C = M.times(MT);
-        return C;
+        return M.times(MT);
     }
+
 }
--- src/KNNClassifier.java
+++ src/KNNClassifier.java
@@ -1,17 +1,94 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by pooler.
  */
-public class KNNClassifier extends Classifier {
 
-    int k;
+class KNNClassifier extends Classifier {
 
-    public KNNClassifier(double[][] dataSet, int[] ClassLabels, int[] SampleCount, int k) {
-        super(dataSet, ClassLabels, SampleCount);
+    private int k;
+
+    KNNClassifier(double[][] dataSet, int[] ClassLabels, int k) {
+        super(dataSet, ClassLabels);
         this.k = k;
     }
 
     @Override
     double execute() {
-        return 0;
+        int match = 0;
+
+        for (int[] i : TestSet) {
+            if (shortDistanceToClassA(dataSet[i[0]])) {
+                if (ClassLabels[i[0]] == 0)
+                    match++;
+            } else {
+                if (ClassLabels[i[0]] == 1)
+                    match++;
+            }
+        }
+        return percent * match / TestSet.length;
+    }
+
+    private boolean shortDistanceToClassA(double[] point) {
+        double[] result;
+        List<Double> distanceA, distanceQ;
+
+        result = new double[2];
+        distanceA = new ArrayList<>();
+        distanceQ = new ArrayList<>();
+
+        for (int[] elementTrainingSet : TrainingSet) {
+            if (ClassLabels[elementTrainingSet[0]] == TRAIN_SET) {
+                distanceA.add(euclidean(point, elementTrainingSet));
+            } else {
+                distanceQ.add(euclidean(point, elementTrainingSet));
+            }
+        }
+
+        double[] double_vector_A = new double[distanceA.size()];
+        for (int i = 0; i < double_vector_A.length; i++) {
+            double_vector_A[i] = distanceA.get(i);
+        }
+
+        double[] double_vector_Q = new double[distanceQ.size()];
+        for (int i = 0; i < double_vector_Q.length; i++) {
+            double_vector_Q[i] = distanceQ.get(i);
+        }
+
+        Arrays.sort(double_vector_A);
+        Arrays.sort(double_vector_Q);
+
+        double[] double_vector_A_to_compare = new double[k];
+        double[] double_vector_Q_to_compare = new double[k];
+
+        for (int i = 0; i < k; i++) {
+            double_vector_A_to_compare[i] = double_vector_A[i];
+            double_vector_Q_to_compare[i] = double_vector_Q[i];
+        }
+
+        int pointer_a = 0;
+        int pointer_q = 0;
+
+        for (double aDouble_vector_A_to_compare : double_vector_A_to_compare) {
+            for (double aDouble_vector_Q_to_compare : double_vector_Q_to_compare) {
+                if (aDouble_vector_A_to_compare > aDouble_vector_Q_to_compare) {
+                    pointer_q++;
+                } else {
+                    pointer_a++;
+                }
+            }
+        }
+
+        double[] winners = new double[]{0.0, 1.0};
+
+        if (pointer_q > pointer_a) {
+            result[1] = winners[1];
+        } else {
+            result[0] = winners[0];
+        }
+
+        return result[0] < result[1];
     }
 }
